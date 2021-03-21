7388a7940b05bf6272f8d22d54d7f0025cf6b3e2
Nik-Sch <nik.schelten@posteo.de>
2021-03-21 15:04:10 +0100

    changed to server-setup

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 [ "$(ls -A $IMAGE_DIRECTORY/)" ] && ./start.sh
-echo "cloning data"
 git clone $BACKUP_REPO $IMAGE_DIRECTORY
 chown -R www-data:www-data $IMAGE_DIRECTORY
 git config --global user.email "rezeptbuch@posteo.de"
 git config --global user.name "Rezeptbuch"
+echo "cloned, waiting 20s for db to come up"
 sleep 20
 mysql -h "$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < $IMAGE_DIRECTORY/backup.sql
 rm $IMAGE_DIRECTORY/backup.sql
+echo "restored backup, starting api"
 
 ./start.sh
