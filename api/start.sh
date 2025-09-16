0b5c9e934ced8f5ae47d3257aab37626a999d45c
Nik-Sch <mail@niklas-schelten.de>
2025-09-16 14:58:05 +0200

    upgrade api

--- api/start.sh
+++ api/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo "[$(date +"%Y-%m-%d %H:%M:%S %z")] Starting in 6s (waiting for db)"
 # sleep 6 # wait for db to come up?
-exec gunicorn app:app
+# exec gunicorn app:app
+[[ -n $DEBUG ]] && exec python3 app.py || exec gunicorn app:app
