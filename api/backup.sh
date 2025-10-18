bdb665b787529525b4359455afa813626941ffdd
Nik-Sch <mail@niklas-schelten.de>
2025-10-18 13:22:55 +0200

    actually fix it

--- api/backup.sh
+++ api/backup.sh
@@ -4,7 +4,7 @@ git config --global user.email "rezeptbuch@posteo.de"
 git config --global user.name "Rezeptbuch"
 echo $(date)
 cd $IMAGE_DIRECTORY
-mysqldump -h $MYSQL_HOST -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > $IMAGE_DIRECTORY/backup.sql
+mysqldump --ssl=off -h $MYSQL_HOST -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > $IMAGE_DIRECTORY/backup.sql
 git add .
 git commit -m "$(date +"%Y-%m-%d %H:%M:%S")"
 git push
